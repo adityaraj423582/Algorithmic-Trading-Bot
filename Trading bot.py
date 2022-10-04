e5c3c49d9b290774329f35b246ecc7a6b49b2840
Aditya Raj Singh <adityaraj423582@gmail.com>
2022-10-05 00:48:09 +0530

    Update Trading bot.py

--- Trading bot.py
+++ Trading bot.py
@@ -7,17 +7,17 @@ from email.mime.text import MIMEText
 
 
 def pairs_trading_algo(self):
-    # Specify paper trading environment
+    
     os.environ["APCA_API_BASE_URL"] = "https://paper-api.alpaca.markets"
-    # Insert API Credentials
-    api = tradeapi.REST('API_KEY_ID', 'API_SECRET_KEY', api_version='v2')  # or use ENV Vars shown below
+    
+    api = tradeapi.REST('API_KEY_ID', 'API_SECRET_KEY', api_version='v2')  
     account = api.get_account()
 
     # The mail addresses and password
     sender_address = 'trading.algo15@gmail.com'
     sender_pass = '**********'
-    receiver_address = 'mcklayne.marshall@gmail.com'
-    # Setup the MIME
+    receiver_address = 'adityaraj1711@gmail.com'
+    
     message = MIMEMultipart()
     message['From'] = 'Trading Bot'
     message['To'] = receiver_address
@@ -27,12 +27,12 @@ def pairs_trading_algo(self):
     days = 1000
     stock1 = 'ADBE'
     stock2 = 'AAPL'
-    # Put Hisrorical Data into variables
+    
     stock1_barset = api.get_barset(stock1, 'day', limit=days)
     stock2_barset = api.get_barset(stock2, 'day', limit=days)
     stock1_bars = stock1_barset[stock1]
     stock2_bars = stock2_barset[stock2]
-    # Grab stock1 data and put in to a array
+    #  array
     data_1 = []
     times_1 = []
     for i in range(days):
@@ -40,7 +40,7 @@ def pairs_trading_algo(self):
         stock1_time = stock1_bars[i].t
         data_1.append(stock1_close)
         times_1.append(stock1_time)
-    # Grab stock2 data and put in to an array
+    # 2  array
     data_2 = []
     times_2 = []
     for i in range(days):
@@ -48,16 +48,16 @@ def pairs_trading_algo(self):
         stock2_time = stock1_bars[i].t
         data_2.append(stock2_close)
         times_2.append(stock2_time)
-    # Putting them together
+    
     hist_close = pd.DataFrame(data_1, columns=[stock1])
     hist_close[stock2] = data_2
-    # Current Spread between the two stocks
+  
     stock1_curr = data_1[days - 1]
     stock2_curr = data_2[days - 1]
     spread_curr = (stock1_curr - stock2_curr)
-    # Moving Average of the two stocks
+  
     move_avg_days = 5
-    # Moving averge for stock1
+   
     stock1_last = []
     for i in range(move_avg_days):
         stock1_last.append(data_1[(days - 1) - i])
@@ -65,42 +65,42 @@ def pairs_trading_algo(self):
     stock1_hist = pd.DataFrame(stock1_last)
 
     stock1_mavg = stock1_hist.mean()
-    # Moving average for stock2
+   
     stock2_last = []
     for i in range(move_avg_days):
         stock2_last.append(data_2[(days - 1) - i])
     stock2_hist = pd.DataFrame(stock2_last)
     stock2_mavg = stock2_hist.mean()
-    # Sread_avg
+
     spread_avg = min(stock1_mavg - stock2_mavg)
-    # Spread_factor
+ 
     spreadFactor = .01
     wideSpread = spread_avg * (1 + spreadFactor)
     thinSpread = spread_avg * (1 - spreadFactor)
-    # Calc_of_shares_to_trade
+    # calculation
     cash = float(account.buying_power)
     limit_stock1 = cash // stock1_curr
     limit_stock2 = cash // stock2_curr
     number_of_shares = int(min(limit_stock1, limit_stock2) / 2)
 
-    # Trading_algo
+    # Trading_algorithm.
     portfolio = api.list_positions()
     clock = api.get_clock()
 
     if clock.is_open == True:
         if bool(portfolio) == False:
-            # detect a wide spread
+            
             if spread_curr > wideSpread:
-                # short top stock
+              
                 api.submit_order(symbol=stock1, qty=number_of_shares, side='sell', type='market', time_in_force='day')
-                # Long bottom stock
+                
                 api.submit_order(symbol=stock2, qty=number_of_shares, side='buy', type='market', time_in_force='day')
                 mail_content = "Trades have been made, short top stock and long bottom stock"
-            # detect a tight spread
+         
             elif spread_curr < thinSpread:
-                # long top stock
+               
                 api.submit_order(symbol=stock1, qty=number_of_shares, side='buy', type='market', time_in_force='day')
-                # short bottom stock
+            
                 api.submit_order(symbol=stock2, qty=number_of_shares, side='sell', type='market', time_in_force='day')
                 mail_content = "Trades have been made, long top stock and short bottom stock"
         else:
@@ -116,11 +116,11 @@ def pairs_trading_algo(self):
     else:
         mail_content = "The Market is Closed"
 
-    # The body and the attachments for the mail
+  
     message.attach(MIMEText(mail_content, 'plain'))
-    # Create SMTP session for sending the mail
-    session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
-    session.starttls()  # enable security
+   
+    session = smtplib.SMTP('smtp.gmail.com', 587)  
+    session.starttls()  
     session.login(sender_address, sender_pass)  # login with mail_id and password
     text = message.as_string()
     session.sendmail(sender_address, receiver_address, text)
